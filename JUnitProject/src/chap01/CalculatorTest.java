33de4b04c4ec6cdf7c73aa60550f669e4050efd6
dmyun23 <dmyun23@gmail.com>
2023-12-16 22:37:59 +0900

    CalculatorTest Junit 프레임워크 이용한 테스트로 변환

--- JUnitProject/src/chap01/CalculatorTest.java
+++ JUnitProject/src/chap01/CalculatorTest.java
@@ -1,26 +1,13 @@
-package chap01;
+package chap01; 
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class CalculatorTest {
 	
-	private int nbErrors = 0;
+	@Test
 	public void testAdd() {
 		Calculator calculator = new Calculator();
 		double result = calculator.add(10, 50);
-		if(result != 60) {
-			throw new IllegalStateException("Bad result: " + result);
-		}
-	}
-	public static void main(String[] args) {
-		CalculatorTest test = new CalculatorTest();
-		try {
-			test.testAdd();
-		}catch(Throwable e) {
-			test.nbErrors++;
-			e.printStackTrace();
-		}
-		if(test.nbErrors > 0) {
-			throw new IllegalStateException("There were " + test.nbErrors + " error(s)");
-		}
-		
+		assertEquals(60,result, 0);
 	}
 }
